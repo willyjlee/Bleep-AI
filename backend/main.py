17b893f24a553c73bf253ccdc548c750eb0f9240
Jay Khatri <jay@joinhandshake.com>
2018-03-03 02:59:16 -0800

    changes to downloads

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,24 @@
-from flask import Flask
+from flask import Flask, request
 from resources.database import hello
+from resources.download import VideoDownloader
+import os
+
 app = Flask(__name__)
+download_handler = VideoDownloader()
 
 @app.route('/')
 def hello_world():
-  hello()
-  return "Bitch this shit workin"
+  return "Welcome Bitch"
+
+@app.route('/download')
+def download():
+  youtube_path = request.args.get('path')
+  print(youtube_path)
+  download_handler.download('https://www.youtube.com/' + youtube_path)
+  return "Installed that shit"
+
+def get_root_path(internal_path=""):
+    return os.path.join(app.root_path, internal_path)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=8080)
