d18c5447fa3725cc90e608f743a6f0af1dee5588
Jay Khatri <jay@joinhandshake.com>
2018-03-03 00:38:07 -0800

    testing file dependency

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from resources.database import hello
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+  hello()
   return "Bitch this shit workin"
 
 if __name__ == '__main__':
---
+++ backend/resources/database.py
@@ -0,0 +1,3 @@
+
+def hello():
+    print("yo bitch")
