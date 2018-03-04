eacf4ce6e51c3fc4329180d3f44c17cdb7171e22
Tejas Shah <tejashah88@gmail.com>
2018-03-03 17:47:44 -0800

    what u say nibba

--- backend/resources/media-services.py
+++ backend/resources/media-services.py
@@ -4,7 +4,7 @@ from azure.storage.blob import ContentSettings
 from azure.storage.blob import PublicAccess
 from variables import blob_account_key
 
-blob_name = 'blobby'
+blob_name = 'blobby.mp4'
 
 # returns token
 def getADToken():
@@ -13,9 +13,7 @@ def getADToken():
     payload = "grant_type=client_credentials&client_id=763758c9-bc20-48de-9b96-65048d646719&client_secret=RqpaGOO7jPGxFCG8oOw%2F3yVWETPm1MgrqIq0uPXSJRg%3D&resource=https%3A%2F%2Frest.media.azure.net"
     headers = {
         'Content-Type': "application/x-www-form-urlencoded",
-        'Keep-Alive': "true",
-        'Cache-Control': "no-cache",
-        'Postman-Token': "9c4bade5-d92c-15d7-5038-ed459ca0d8f9"
+        'Keep-Alive': "true"
     }
 
     response = requests.request("POST", url, data=payload, headers=headers)
@@ -34,9 +32,7 @@ def createAsset(token, name):
         'DataServiceVersion': "3.0",
         'MaxDataServiceVersion': "3.0",
         'User-Agent': "azure media services postman collection",
-        'Authorization': "Bearer %s" % token,
-        'Cache-Control': "no-cache",
-        'Postman-Token': "54cf30c0-b454-a860-b8af-1c33a1ab1b53"
+        'Authorization': "Bearer %s" % token
     }
 
     response = requests.request("POST", url, data=payload, headers=headers)
@@ -55,9 +51,7 @@ def createSASLocator(token, assetId):
         'DataServiceVersion': "3.0",
         'MaxDataServiceVersion': "3.0",
         'User-Agent': "azure media services postman collection",
-        'Authorization': "Bearer %s" % token,
-        'Cache-Control': "no-cache",
-        'Postman-Token': "2a6a5229-a2c3-ab36-1610-d508a907db33"
+        'Authorization': "Bearer %s" % token
     }
 
     response = requests.request("POST", url, data=payload, headers=headers)
@@ -67,7 +61,6 @@ def createSASLocator(token, assetId):
 def uploadInputFile(uploadUrl, filepath):
     block_blob_service = BlockBlobService(account_name='wordsplitter', account_key=blob_account_key)
     block_blob_service.create_blob_from_path('asset-d63d7b5a-9d33-4ff2-b8b7-9153ec48e0fd', blob_name, filepath, content_settings=ContentSettings(content_type='video/mp4'))
-
 
 def addMetadata(assetId):
     url = "https://wordsplitter.restv2.centralus.media.azure.net/api/CreateFileInfos"
@@ -87,6 +80,27 @@ def addMetadata(assetId):
 
     requests.request("GET", url, headers=headers, params=querystring)
 
+# returns Job ID
+def startIndexingJob(token, assetId):
+    url = "https://wordsplitter.restv2.centralus.media.azure.net/api/Jobs"
+
+    payload = "{\"Name\":\"Indexer v2 HackTech Job\",\"InputMediaAssets\":[{\"__metadata\":{\"uri\": \"https://wordsplitter.restv2.centralus.media.azure.net/api//Assets('%s')\"}}],\"Tasks\":[{\"Configuration\":\"{'Version':'1.0','Features':[{'Options':{'Formats':['WebVtt','TTML'],'Language':'EnUs','Type':'RecoOptions'},'Type':'SpReco'}]}\",\"MediaProcessorId\":\"nb:mpid:UUID:1927f26d-0aa5-4ca1-95a3-1a3f95b0f706\",\"TaskBody\": \"<?xml version=\\\"1.0\\\" encoding=\\\"utf-8\\\"?><taskBody><inputAsset>JobInputAsset(0)</inputAsset><outputAsset assetName=\\\"foobar.mp4\\\">JobOutputAsset(0)</outputAsset></taskBody>\"}]}" % assetId
+
+    headers = {
+        'x-ms-version': "2.15",
+        'Accept': "application/json;odata=verbose",
+        'Content-Type': "application/json;odata=verbose",
+        'DataServiceVersion': "3.0",
+        'MaxDataServiceVersion': "3.0",
+        'User-Agent': "azure media services postman collection",
+        'Authorization': "Bearer %s" % token
+    }
+
+    response = requests.request("POST", url, data=payload, headers=headers)
+    return response.json()["d"]["Id"]
+
+
+
 def processVideo(videoPath):
     token = getADToken()
     assetId = createAsset(token, "lolwatchmenow")
@@ -94,6 +108,6 @@ def processVideo(videoPath):
     uploadUrl = createSASLocator(token, assetId)
     uploadInputFile(uploadUrl, videoPath)
     addMetadata(assetId)
-
-processVideo('/Users/lee/Documents/pprojects/HackTech2018/backend/video.mp4')
+    jobId = startIndexingJob(token, assetId)
+    print(jobId)
 
